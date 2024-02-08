b3709eaa98403a151ea480b4d7cf644fa0e35efc
chalameg <chalamegersa5@gmail.com>
2024-02-08 17:12:24 +0300

    httpService baseUrl updated

--- frontend/src/httpService.ts
+++ frontend/src/httpService.ts
@@ -1,7 +1,7 @@
 // src/api/httpService.js or src/api/api.js
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const baseURL = 'https://bookstorebackend-ii1w.onrender.com/api';
 
 const httpService = axios.create({
   baseURL: baseURL,
